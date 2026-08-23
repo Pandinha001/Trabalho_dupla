211f68b772657b6de87ae5a84339d93ddf913d92
Pietro Machado <pietromacmiranda@gmail.com>
2026-08-23 13:46:49 -0300

    Implement methods for adding and searching in Hospital
    Added methods for adding and searching individuals in the Hospital class.

--- hospital.py
+++ hospital.py
@@ -1,3 +1,15 @@
 class Hospital():
-    def __init__(self, remedio, exame, data_entrada):
+    def __init__(self):
         pass
+    def adicionar():
+        pass
+
+        # salvar em listas os medicos, pacientes e enfermeiros 
+
+    def buscar_pessoa(codigo_identificacao):
+        
+        pass
+
+    # conferir nas listas se o codigo de identificacao bate,  e se 
+    # necessario separar para n ter confusao nas listas, 
+    # se necessario mudar o nome das variaveis q deixei igual
