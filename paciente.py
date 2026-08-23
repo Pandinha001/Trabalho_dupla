567344b9e2e0d9252e76bf49f87b154642521683
Pietro Machado <pietromacmiranda@gmail.com>
2026-08-23 13:50:22 -0300

    Refactor Paciente class constructor and methods
    Removed 'historico_medico' from constructor and added identification code generation.

--- paciente.py
+++ paciente.py
@@ -1,15 +1,31 @@
+from pessoa import Pessoa
 
 pacientes = {}
 class Paciente(Pessoa):
-    def __init__(self, nome, telefone, email, data_nasc, cpf, sintoma, historico_medico, tipo_sanguineo):
+
+    contador = 0
+    def __init__(self, nome, telefone, email, data_nasc, cpf, sintoma, tipo_sanguineo):
         super().__init__(nome, telefone, email, data_nasc, cpf)
-
         self.sintoma = sintoma
-        self.historico_medico = historico_medico
         self.tipo_sanguineo = tipo_sanguineo
-
+        
+        self.codigo_identificacao = self.codigo_identificacao()
+        
     def GerarIdentinficador(self):
-        pass
+        print(self.nome)
+        print(self.telefone)
+        print(self.email)
+        print(self.data_nasc)
+        print(self.cpf)
+        print(self.tipo_sanguineo)
+        
+    def codigo_identificacao(self):
 
+        Paciente.contador += 1
+        return Paciente.contador
+        
     def Agendar_consulta(self):
-        pass
+        print("OLA! VAMOS COMEÇAR SEU AGENDAMENTO")
+        cod_consulta = int(input("informe seu código de identificação:"))
+        especialista = input("informe o especialista: (cardiologista, pediatra, clinico geral, cirurgiao) ")
+        data_consulta = input(" informe a data da consulta (dd/mm/yy)") 
