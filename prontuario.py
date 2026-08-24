395b668cc479b7bf9c5ca878e5a9401f11c73e3c
Pietro Machado <pietromacmiranda@gmail.com>
2026-08-24 13:21:56 -0300

    Refactor Prontuario class constructor and methods
    Removed tipo_sanguineo from constructor and print method.

--- prontuario.py
+++ prontuario.py
@@ -2,11 +2,11 @@ from paciente import Paciente
 
 class Prontuario(Paciente):
     def __init__(self,  nome, telefone, email, data_nasc, cpf, sintoma, tipo_sanguineo, exame, data_entrada):
-        super().__init__(nome, telefone, email, data_nasc, cpf,)
+        super().__init__(nome, telefone, email, data_nasc, cpf, tipo_sanguineo)
         self._exame = exame
         self._data_entrada = data_entrada
         self._sintoma = sintoma
-        self._tipo_sanguineo = tipo_sanguineo
+        
 
     def Imprimir_prontuario(self):
 
@@ -15,6 +15,5 @@ class Prontuario(Paciente):
         print("cpf:", self._cpf)
         print("sintoma:", self._sintoma)
         print("exame:",self._exame)
-        print("remedio:",self._remedio)
         print("tipo saguíneo:", self._tipo_sanguineo)
         print("data entrada", self._data_entrada)
