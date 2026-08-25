c45edee38913fe2c03409c9fbc7aff5c1bc1ccd0
Pietro Machado <pietromacmiranda@gmail.com>
2026-08-24 21:58:28 -0300

    Refactor Prontuario class to remove inheritance

--- prontuario.py
+++ prontuario.py
@@ -1,16 +1,17 @@
-from paciente import Paciente
 
-class Prontuario(Paciente):
-    def __init__(self,  nome, telefone, email, data_nasc, cpf, sintoma, tipo_sanguineo, exame, data_entrada):
-        super().__init__(nome, telefone, email, data_nasc, cpf, tipo_sanguineo)
+
+class Prontuario():
+    def __init__(self,  sintomas, tipo_sanguineo, exame, data_entrada, paciente):
+        
         self._exame = exame
         self._data_entrada = data_entrada
-        self._sintoma = sintoma
-
+        self._sintoma = sintomas
+        self.p =  paciente
+        self._tipo_sanguineo = tipo_sanguineo
     def Imprimir_prontuario(self):
         print("PRONTUÁRIO MÉDICO")
-        print("Nome:", self._nome )
-        print("cpf:", self._cpf)
+        print("Nome:", self.p._nome )
+        print("cpf:", self.p._cpf)
         print("sintoma:", self._sintoma)
         print("exame:",self._exame)
         print("tipo saguíneo:", self._tipo_sanguineo)
